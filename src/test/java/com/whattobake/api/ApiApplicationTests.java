7080bf31316d313fe2c727ca6d7d8aab37fed707
poneciak <poneciak@gmail.com>
2022-12-16 20:29:54 +0100

    Initial Commit

---
+++ src/test/java/com/whattobake/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whattobake.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
