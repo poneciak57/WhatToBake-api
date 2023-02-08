40f71d74b57c47e2d5c1b3848bd7cffa2763f759
poneciak <poneciak@gmail.com>
2023-02-08 21:16:50 +0100

    exception handling
    added exception handling for authentication and moved pbUrl to properties

--- src/main/java/com/whattobake/api/Security/AuthenticationManager.java
+++ src/main/java/com/whattobake/api/Security/AuthenticationManager.java
@@ -1,6 +1,7 @@
 package com.whattobake.api.Security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
@@ -17,6 +18,10 @@ import java.util.stream.Collectors;
 @Component
 @Slf4j
 public class AuthenticationManager implements ReactiveAuthenticationManager {
+
+    @Value("${pocketbase.url}")
+    private String pocketbaseURL;
+
     @Override
     public Mono<Authentication> authenticate(Authentication authentication) {
         String authToken = authentication.getCredentials().toString();
@@ -32,17 +37,23 @@ public class AuthenticationManager implements ReactiveAuthenticationManager {
                 });
     }
 
-    private Mono<PbUser> getUser(String token){
+    private Mono<PbUser> getUser(String token) {
 //      INFO Auth token Should be as follows "uid|jwtToken";
-        String[] tt = token.split("\\|");
-        String uid = tt[0];
-        String auth = tt[1];
+        String auth;
+        String uid;
+        try {
+            String[] tt = token.split("\\|");
+            uid = tt[0];
+            auth = tt[1];
+        } catch (Exception e) {
+            return Mono.empty();
+        }
         WebClient client = WebClient.builder()
-                .baseUrl("http://132.226.204.66:82")
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer "+auth)
+                .baseUrl(pocketbaseURL)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + auth)
                 .build();
         return client.get()
-                .uri("/api/collections/users/records/"+uid)
+                .uri("/api/collections/users/records/" + uid)
                 .exchangeToMono(response -> {
                     if (response.statusCode().equals(HttpStatus.OK)) {
                         return response.bodyToMono(PbUser.class);
