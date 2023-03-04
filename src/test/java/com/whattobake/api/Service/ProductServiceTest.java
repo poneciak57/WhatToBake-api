1210aa65996a07102c1b076dec7807f65984b05c
Kacper Poneta <poneciak@gmail.com>
2023-03-04 13:45:21 +0100

    implemented product service unit tests

--- src/test/java/com/whattobake/api/Service/ProductServiceTest.java
+++ src/test/java/com/whattobake/api/Service/ProductServiceTest.java
@@ -2,6 +2,7 @@ package com.whattobake.api.Service;
 
 
 import com.whattobake.api.Dto.FilterDto.ProductFilters;
+import com.whattobake.api.Exception.NodeNotFound;
 import com.whattobake.api.Repository.ProductRepository;
 import com.whattobake.api.Util.ProductCreator;
 import com.whattobake.api.Util.TagCreator;
@@ -19,7 +20,9 @@ import reactor.blockhound.BlockingOperationError;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
 
+import java.util.Optional;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
@@ -46,55 +49,80 @@ class ProductServiceTest {
     @Test
     @DisplayName("all, when filters are default should return flux of products")
     public void testAllProducts_whenFiltersAreOk_thenReturnFluxOfProducts(){
-
+        StepVerifier.create(productService.getAllProducts(Optional.of(ProductCreator.defaultFilters())))
+                .expectSubscription()
+                .expectNext(ProductCreator.valid())
+                .verifyComplete();
     }
 
     @Test
     @DisplayName("all, when filters are null, should return flux of products")
     public void testAllProducts_whenFiltersAreNull_thenReturnFluxOfProducts(){
-
+        StepVerifier.create(productService.getAllProducts(Optional.empty()))
+                .expectSubscription()
+                .expectNext(ProductCreator.valid())
+                .verifyComplete();
     }
 
     @Test
     @DisplayName("one_by_id, should return mono of product")
     public void testOneById_whenIdIsCorrect_thenReturnMonoOfProduct(){
-
+        StepVerifier.create(productService.getOneById(ProductCreator.VALID_ID))
+                .expectSubscription()
+                .expectNext(ProductCreator.valid())
+                .verifyComplete();
     }
 
     @Test
     @DisplayName("one_by_id, should throw an error")
     public void testOneById_whenIdIsIncorrect_thenThrowException(){
-
+        StepVerifier.create(productService.getOneById(ProductCreator.INVALID_ID))
+                .expectSubscription()
+                .verifyError(NodeNotFound.class);
     }
 
     @Test
     @DisplayName("update, should return mono of product")
     public void testUpdateProduct_whenIdIsCorrect_thenReturnMonoOfProduct(){
-
+        StepVerifier.create(productService.updateProduct(ProductCreator.validUpdate()))
+                .expectSubscription()
+                .expectNext(ProductCreator.valid())
+                .verifyComplete();
     }
 
     @Test
     @DisplayName("update, should throw an error")
     public void testUpdateProduct_whenIdIsIncorrect_thenThrowException(){
-
+        StepVerifier.create(productService.updateProduct(ProductCreator.invalidUpdate()))
+                .expectSubscription()
+                .verifyError(NodeNotFound.class);
     }
 
     @Test
     @DisplayName("delete, should return mono empty")
     public void testDeleteProduct_whenIdIsCorrect_thenReturnMonoEmpty(){
-
+        StepVerifier.create(productService.deleteProduct(ProductCreator.VALID_ID))
+                .expectSubscription()
+                .verifyComplete();
+        Mockito.verify(productRepository,Mockito.times(1)).delete(ArgumentMatchers.any());
     }
 
     @Test
     @DisplayName("delete, should throw an error")
     public void testDeleteProduct_whenIdIsIncorrect_thenThrowException(){
-
+        StepVerifier.create(productService.deleteProduct(ProductCreator.INVALID_ID))
+                .expectSubscription()
+                .verifyError(NodeNotFound.class);
+        Mockito.verify(productRepository,Mockito.never()).delete(ArgumentMatchers.any());
     }
 
     @Test
     @DisplayName("create, should return mono of product")
     public void testNewProduct_whenOK_thenReturnMonoOfProduct(){
-
+        StepVerifier.create(productService.newProduct(ProductCreator.validInsert()))
+                .expectSubscription()
+                .expectNext(ProductCreator.valid())
+                .verifyComplete();
     }
 
     @Test
