7d5a96e0e94a2b93e671b352d27c8cb231daf201
Kacper Poneta <poneciak@gmail.com>
2023-04-07 15:45:54 +0200

    bug/tagController doesnt have /api prefix

--- src/main/java/com/whattobake/api/Controller/TagController.java
+++ src/main/java/com/whattobake/api/Controller/TagController.java
@@ -24,7 +24,7 @@ import reactor.core.publisher.Mono;
 
 @io.swagger.v3.oas.annotations.tags.Tag(name ="4. Tag")
 @RestController
-@RequestMapping("/tag")
+@RequestMapping("/api/tag")
 @RequiredArgsConstructor
 @Validated
 public class TagController {
