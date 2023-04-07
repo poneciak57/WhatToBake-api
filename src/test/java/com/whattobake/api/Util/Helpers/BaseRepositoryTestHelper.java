1fb7c32517a32f73be14e669998b2973f05b21ae
Kacper Poneta <poneciak@gmail.com>
2023-04-07 13:47:21 +0200

    added repository tests

--- src/test/java/com/whattobake/api/Util/Helpers/BaseRepositoryTestHelper.java
+++ src/test/java/com/whattobake/api/Util/Helpers/BaseRepositoryTestHelper.java
@@ -1,14 +1,17 @@
 package com.whattobake.api.Util.Helpers;
 
+import com.whattobake.api.Dto.InsertDto.RatingInsertRequest;
 import com.whattobake.api.Model.Category;
 import com.whattobake.api.Model.Product;
 import com.whattobake.api.Model.Recipe;
 import com.whattobake.api.Model.Tag;
 import com.whattobake.api.Repository.CategoryRepository;
 import com.whattobake.api.Repository.Implementations.LikeRepositoryImpl;
+import com.whattobake.api.Repository.Implementations.RatingRepositoryImpl;
 import com.whattobake.api.Repository.Implementations.RecipeRepositoryImpl;
 import com.whattobake.api.Repository.LikeRepository;
 import com.whattobake.api.Repository.ProductRepository;
+import com.whattobake.api.Repository.RatingRepository;
 import com.whattobake.api.Repository.RecipeRepository;
 import com.whattobake.api.Repository.TagRepository;
 import com.whattobake.api.Util.Creators.CategoryCreator;
@@ -23,7 +26,7 @@ import org.springframework.data.neo4j.core.ReactiveNeo4jClient;
 
 import java.util.List;
 
-@Import({RecipeRepositoryImpl.class, LikeRepositoryImpl.class})
+@Import({RecipeRepositoryImpl.class, LikeRepositoryImpl.class, RatingRepositoryImpl.class})
 public abstract class BaseRepositoryTestHelper extends BaseRepositoryTestTestcontainers {
 
     @Autowired
@@ -42,6 +45,9 @@ public abstract class BaseRepositoryTestHelper extends BaseRepositoryTestTestcon
     protected LikeRepository likeRepository;
 
     @Autowired
+    protected RatingRepository ratingRepository;
+
+    @Autowired
     protected ReactiveNeo4jClient client;
 
     @BeforeEach
@@ -55,11 +61,16 @@ public abstract class BaseRepositoryTestHelper extends BaseRepositoryTestTestcon
         Product p2 = createProduct(c1);
         Tag t1 = createTag();
         Recipe recipe = createRecipe(List.of(p1, p2), List.of(t1));
-        return likeRecipe(recipe.getId(), UserCreator.VALID_ID);
+        likeRecipe(recipe.getId(), UserCreator.VALID_ID);
+        return rateRecipe(3L, recipe.getId(), UserCreator.VALID_ID);
     }
 
     protected Recipe likeRecipe(Long recipeId, String uid) {
         return likeRepository.like(recipeId, uid).block();
+    }
+
+    protected Recipe rateRecipe(Long stars, Long recipeId, String pbuid) {
+        return ratingRepository.addRating(new RatingInsertRequest(stars), recipeId, pbuid).block();
     }
 
     protected Recipe createRecipe(List<Product> products, List<Tag> tags) {
